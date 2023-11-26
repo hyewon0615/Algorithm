4de4bb8d5ea60816dedc98f2577a0424a65b5eee
hyewon <gpdnjs154@navr.com>
2023-11-26 18:51:02 +0900

    자릿수더하기

--- 07.js
+++ 07.js
@@ -1,9 +1,13 @@
-function solution(n){
-    return (n+"").split("").reduce((acc, curr) => acc + Number(curr), 0)
-    
+//자릿수 더하기
+//자연수 N이 주어지면, N의 각 자릿수의 합을 구해서 return 하는 solution 함수를 만들어 주세요.
+//예를들어 N = 123이면 1 + 2 + 3 = 6을 return 하면 됩니다.
+//https://school.programmers.co.kr/learn/courses/30/lessons/12931
+
+function solution(n) {
+  return (n + "").split("").reduce((acc, curr) => acc + Number(curr), 0);
 }
 
-console.log(solution(52451))
+console.log(solution(52451));
 // console.log(acc)
 // 5 2
 // 7 4
